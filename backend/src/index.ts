7a2241f1fe2bcbd71924007069292dc9cd90b346
25LP0861 25LP0861 <25LP0861@pinecones-iMac-2.local>
2025-07-07 15:16:08 +0800

    p

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import foodsRouter from "./routes/food.route.js";
+import orderRouter from "./routes/order.route.js";
+import categoryRouter from "./routes/category.route.js";
+import userRouter from "./routes/user.route.js";
+import foodOrderItemRouter from "./routes/foodOrderItems.route.js";
+
+dotenv.config();
+
+mongoose.connect((process.env.MONGO_URL as string) || "");
+
+const server = express();
+server.use(express.json());
+
+const port = process.env.PORT || "";
+
+server.use("/food", foodsRouter);
+server.use("/order", orderRouter);
+server.use("/category", categoryRouter);
+server.use("/user", userRouter);
+server.use("/items", foodOrderItemRouter);
+
+server.get("/", (_request, response) => {
+  response.send("Hello zaya");
+});
+
+server.listen(port, () => {
+  console.log(`Server aslaa ${port}`);
+});
---
+++ backend/src/routes/order.route.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getAllOrders,
+  createOrder,
+  getOrder,
+  //   updateOrder,
+} from "../controllers/order.controller.js";
+
+const orderRouter = express.Router();
+
+orderRouter.get("/", getAllOrders);
+orderRouter.get("/:id", getOrder);
+orderRouter.post("/", createOrder);
+// orderRouter.put("/:orderId", updateOrder);
+
+export default orderRouter;
